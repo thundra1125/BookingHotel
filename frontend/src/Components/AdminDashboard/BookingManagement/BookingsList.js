b75643dc47c9188b02efe746b7734166c7b5854d
pitLord1125 <mickeyglass.1125@gmail.com>
2022-06-27 09:18:16 +0900

    weg

--- frontend/src/Components/AdminDashboard/BookingManagement/BookingsList.js
+++ frontend/src/Components/AdminDashboard/BookingManagement/BookingsList.js
@@ -16,36 +16,41 @@ function BookingsList() {
     }, []); // eslint-disable-line
 
     return (
-        <div className="w-full md:w-6/12 xl:w-8/12 md:ml-5 bg-gray-200 p-5 rounded-sm  ">
-            <h2 className="text-xl font-semibold">All Bookings</h2>
-            {state.bookings.loading && <Loading />}
-            {state.bookings.pagination.last_page !== 1 && (
-                <Pagination
-                    paginate={state.bookings}
-                    method={getAllBookings}
-                    params={[dispatch, state.auth.token]}
-                />
-            )}
+      <div className="w-full md:w-6/12 xl:w-8/12 md:ml-5 bg-gray-200 p-5 rounded-sm  ">
+        <h2 className="text-xl font-semibold">All Bookings</h2>
+        {state.bookings.loading && <Loading />}
+        {state.bookings.pagination.last_page !== 1 && (
+          <Pagination
+            paginate={state.bookings}
+            method={getAllBookings}
+            params={[dispatch, state.auth.token]}
+          />
+        )}
+        {state.bookings.pagination.last_page !== 1 && (
+          <Pagination
+            paginate={state.bookings}
+            method={getAllBookings}
+            params={[dispatch, state.auth.token]}
+          />
+        )}
 
-            <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
-                {state &&
-                    state.bookings.allBookings.map((booking) => {
-                        return (
-                            <BookingItem key={booking.id} booking={booking} />
-                        );
-                    })}
-            </div>
-            {state.bookings.allBookings.length === 0 && (
-                <div className="mt-10">There are no bookings!</div>
-            )}
-            {state.bookings.pagination.last_page !== 1 && (
-                <Pagination
-                    paginate={state.bookings}
-                    method={getAllBookings}
-                    params={[dispatch, state.auth.token]}
-                />
-            )}
+        <div className="grid grid-cols-1 gap-4 xl:grid-cols-2">
+          {state &&
+            state.bookings.allBookings.map((booking) => {
+              return <BookingItem key={booking.id} booking={booking} />;
+            })}
         </div>
+        {state.bookings.allBookings.length === 0 && (
+          <div className="mt-10">There are no bookings!</div>
+        )}
+        {state.bookings.pagination.last_page !== 1 && (
+          <Pagination
+            paginate={state.bookings}
+            method={getAllBookings}
+            params={[dispatch, state.auth.token]}
+          />
+        )}
+      </div>
     );
 }
 
