bc13b6560dd8dca3b67d382f8e89e687302cb78a
webmaster1125 <mickeyglass.1125@gmail.com>
2022-07-16 11:27:28 +0900

    editbadge

--- frontend/src/Components/AdminDashboard/RoomManagement/AddRoom.js
+++ frontend/src/Components/AdminDashboard/RoomManagement/AddRoom.js
@@ -10,26 +10,26 @@ import { useSelector } from "react-redux";
 import Loading from "../../Global/Loading";
 
 function AddRoom() {
-    const state = useSelector((state) => state);
+  const state = useSelector((state) => state);
 
-    return (
-        <>
-            <Header />
-            {state.rooms.loading && <Loading />}
-            {state.rooms.success === false && (
-                <ErrorMessage errors={state.rooms.errors} />
-            )}
-            {state.rooms.success && <SuccessMessage message="Success" />}
-            <main className="bg-gray-100 min-h-screen flex flex-col md:flex-row md:justify-center p-5 text-gray-800">
-                <AdminSiderBar />
-                <AdminDropDown />
+  return (
+    <>
+      <Header />
+      {state.rooms.loading && <Loading />}
+      {state.rooms.success === false && (
+        <ErrorMessage errors={state.rooms.errors} />
+      )}
+      {state.rooms.success && <SuccessMessage message="Success" />}
+      <main className="bg-gray-100 min-h-screen flex flex-col md:flex-row md:justify-center p-5 text-gray-800">
+        <AdminSiderBar />
+        <AdminDropDown />
 
-                <AddRoomForm />
-            </main>
+        <AddRoomForm />
+      </main>
 
-            <Footer />
-        </>
-    );
+      <Footer />
+    </>
+  );
 }
 
 export default AddRoom;
