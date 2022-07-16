f551ee0f467884cb6311a8c7f309bb377e740725
webmaster1125 <mickeyglass.1125@gmail.com>
2022-07-16 11:38:02 +0900

    hotelItem

--- frontend/src/Components/AdminDashboard/AdminProfile/AdminProfileMain.js
+++ frontend/src/Components/AdminDashboard/AdminProfile/AdminProfileMain.js
@@ -11,39 +11,39 @@ import { setSuccess } from "../../../redux/actions/global";
 import { useParams } from "react-router-dom";
 
 function AdminProfileMain() {
-    const dispatch = useDispatch();
-    const state = useSelector((state) => state);
-    let { id } = useParams();
+  const dispatch = useDispatch();
+  const state = useSelector((state) => state);
+  let { id } = useParams();
 
-    useEffect(() => {
-        getUser(dispatch, state.auth.token, id);
-        document.title = `Profile`;
-    }, []); // eslint-disable-line
+  useEffect(() => {
+    getUser(dispatch, state.auth.token, id);
+    document.title = `Profile`;
+  }, []); // eslint-disable-line
 
-    useEffect(() => {
-        const timer = setTimeout(() => {
-            setSuccess(dispatch, null);
-        }, 3000);
-        return () => clearTimeout(timer);
-    }, [state.users.success]); // eslint-disable-line
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setSuccess(dispatch, null);
+    }, 3000);
+    return () => clearTimeout(timer);
+  }, [state.users.success]); // eslint-disable-line
 
-    return (
-        <div className="w-full md:w-6/12 xl:w-8/12 md:ml-5 bg-gray-200 p-5 rounded-sm  rounded-t-none md:rounded-t-md">
-            {state.users.loading && <Loading />}
+  return (
+    <div className="w-full md:w-6/12 xl:w-8/12 md:ml-5 bg-gray-200 p-5 rounded-sm  rounded-t-none md:rounded-t-md">
+      {state.users.loading && <Loading />}
 
-            {state.users.success === false && (
-                <ErrorMessage errors={state.users.errors} />
-            )}
-            {state.users.success && <SuccessMessage message="Success" />}
-            {state.users && (
-                <>
-                    <AdminPersonalInfo user={state.users.user} id={id} />
-                    <AdminEmail id={id} />
-                    <AdminPassword id={id} />
-                </>
-            )}
-        </div>
-    );
+      {state.users.success === false && (
+        <ErrorMessage errors={state.users.errors} />
+      )}
+      {state.users.success && <SuccessMessage message="Success" />}
+      {state.users && (
+        <>
+          <AdminPersonalInfo user={state.users.user} id={id} />
+          <AdminEmail id={id} />
+          <AdminPassword id={id} />
+        </>
+      )}
+    </div>
+  );
 }
 
 export default AdminProfileMain;
